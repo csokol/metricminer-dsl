f52e81a0fe02861f470689f0646215705e29cec0
Francisco Sokol <chico.sokol@gmail.com>
2013-01-29 22:01:35 -0200

    commit properties mapped

--- app/model/commit.rb
+++ app/model/commit.rb
@@ -1,11 +1,17 @@
-class Commit 
-  include DataMapper::Resource
+class Commit
 
+  include DataMapper::Resource
+  
   property :id, Serial
   property :commit_id, String
+  property :date, DateTime
   property :prior_commit_id, String
-  property :date, DateTime 
 
   belongs_to :project, :child_key => [:project_id], :field => 'project_id'
+
+  # todo: map these relationships
+  #property :author_id, Integer
+  #property :message_id, Integer
+  #property :diff_id, Integer
 
 end
